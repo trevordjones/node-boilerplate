eedd2da9f71a4aac343c6772d2023d3875006432
Trevor Jones <trevordavidjones@gmail.com>
2016-08-05 11:48:06 -0400

    Added MongoDB database
    Also included:
    cookie parser
    morgan for printing requests to log
    flash messages
    body parser for reading info from forms

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
 app.use(express.static(__dirname + '/app'));
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
 app.set('view engine', 'jade');
 
 var routes = require('./config/routes')
